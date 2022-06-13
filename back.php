208b809f11421868569c00e6bb06352865d40009
hsinyichang <f1233772002@gmail.com>
2022-06-13 14:44:53 +0800

    增加對include檔案的判斷

--- back.php
+++ back.php
@@ -17,10 +17,11 @@
 <div id="container">
 <?php
 if(isset($_GET['do'])){
-    $file="./back/".$_GET['do'].".php";
-    // 按下button 觸發onclick事件 帶入query string
-    // 有GET到do 這個參數↓
-    include $file;   // 則include 變數file這個檔案，直接把此檔案放進來這個頁面
+    $file="./back/".$_GET['do'].".php";// 按下button 觸發onclick事件 帶入query string
+                                    // 有GET到do這個參數↓
+    if(isset($file) && file_exists($file)){//先檢查是否有file此變數且此檔案是否存在
+        include $file;// 則include 變數file這個檔案，直接把此檔案放進來這個頁面
+    }   
 }else{
 ?>
     <button class="btn btn-primary" onclick="location.href='?do=add_vote'">新增投票</button>
