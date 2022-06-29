723cc59370ff10b55710c1facbc9e8bf6832aaf5
HsinYiChang <f1233772002@gmail.com>
2022-06-29 12:12:37 +0800

    修改前後台登入登出顯示

--- layout/back_nav.php
+++ layout/back_nav.php
@@ -2,5 +2,15 @@
         <div><a href="back.php"><i class="fa-solid fa-bars"></i>投票列表</a></div>
         <div><a href="?do=add_vote"><i class="fa-solid fa-plus"></i>新增投票</a></div>
         <div><a href="?do=admin_type"><i class="fa-solid fa-database"></i>分類管理</a></div>
-        <div><a href="../login/logout.php"><i class="fa-solid fa-circle-user"></i>登出</a></div>
-    </nav>
+        <?php if(isset($_SESSION['admin'])){
+        ?>
+            <div><a href="../login/logout.php"><i class="fa-solid fa-circle-user"></i>登出</a></div>
+        <?php
+        }else{
+        ?>
+        <div><a href="../backlogin/login.php"><i class="fa-solid fa-circle-user"></i>登入</a></div>
+    <?php
+    }
+    ?>
+
+        </nav>
--- layout/front_nav.php
+++ layout/front_nav.php
@@ -1,5 +1,6 @@
 <nav>
         <div><a href="index.php"><i class="fa-solid fa-house"></i>回首頁</a></div>
+        <div><a href="index.php"><i class="fa-solid fa-bars"></i>投票列表</a></div>
         <?php
         // include_once "connect.php";/*放到程式開頭，因裡面有session start */
         if(isset($_SESSION['user'])){/*這裡判斷是否有session為登出或登入 */
@@ -12,7 +13,16 @@
         <?php    
         }
         ?>
+
+        <?php
+        // include_once "connect.php";/*放到程式開頭，因裡面有session start */
+        if(!isset($_SESSION['user'])){/*這裡判斷是否有session為登出或登入 */
+        ?>
+    <div><a href="../backlogin/login.php"><i class="fa-solid fa-circle-user"></i>後台管理</a></div>
+        <?php
+        }else{
+           
+        }
+        ?>
         
-        
-        <div><a href="index.php"><i class="fa-solid fa-bars"></i>投票列表</a></div>
     </nav>
