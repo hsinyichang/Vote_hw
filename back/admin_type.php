36790e69a28911ce169a3d8a89945b0f4577d974
HsinYiChang <f1233772002@gmail.com>
2022-07-01 11:37:30 +0800

    將新增分類做操作選項，可刪除分類

--- back/admin_type.php
+++ back/admin_type.php
@@ -1,9 +1,32 @@
+<?php
+
+include_once "./api/base.php";
+?>
 <form action="../api/add_type.php" method="post">
     <div>
-        <label for="name">分類名稱:</label>
+        <label for="name">新增分類:</label>
         <input type="text" name="name" id="name">
     </div>
     <div>
         <input class="btn btn-primary" type="submit" value="送出">
     </div>
 </form>
+<div>
+    <table id="table">
+        <tr id="list-header">
+            <td>分類名稱</td>
+            <td>操作</td>
+        </tr>
+        <?php
+        $types=all('types');
+        foreach($types as $type){
+            echo "<tr class='list-items'>";
+            echo "<td>{$type['name']}</td>";
+            echo "<td class='text-center'>";//操作區
+                echo "<a class='del' href='?do=deltype&id={$type['id']}'>刪除</a>";
+                echo "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</div>
