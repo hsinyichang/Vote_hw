064cb46ab74a9015b089b73e5d4c4943d2acb5b5
HsinYiChang <f1233772002@gmail.com>
2022-07-04 11:17:45 +0800

    增加開始結束時間選擇以及動態刪除欄位修改

--- back/add_vote.php
+++ back/add_vote.php
@@ -1,42 +1,79 @@
-<form action="./api/add_vote.php" method="post">
-<div><label for="types">選擇分類：</label>
-        <select name="types" id="types">
-        <?php
-            $types=all("types");
-            foreach($types as $type){
-                echo "<option value='{$type['id']}'>";
-                echo $type['name'];
-                echo "</option>";
-            }
-            ?>
-        </select>
-    </div>
-
-    <div>
-        <label for="subject">投票主題：</label>
-        <input type="text" name="subject" id="subject">
-        
-    </div>
-    <div id="selector">
-        <input type="radio" name="multiple" value="0" checked>
-        <label>單選</label>
-        <input type="radio" name="multiple" value="1" >
-        <label>複選</label>
-    </div>
-    <div id="options">
-        <div>
-            <label>選項:</label><input type="text" name="option[]"><input type="button" value="新增選項" id="addvote">
-        </div>
-    </div>
-    <input type="submit" value="新增">
-
-</form>
-<script>
-    $('#addvote').click(function(){
-                //jq
-                const div1=$('#options'); 
-                const addv=$('<div><label>選項:</label><input type="text" name="option[]"></div>');//建立 p元素+text jq node
-                div1.append(addv);//把節點放進來(子層)
-            });
+<form action="" method="post">
+    <table>
+        <tr>
+            <td>
+                <label for="types">選擇分類：</label>
+            </td>
+            <td>
+                <select name="types" id="types">
+                <?php
+                    $types=all("types");
+                    foreach($types as $type){
+                    echo "<option value='{$type['id']}'>";
+                    echo $type['name'];
+                    echo "</option>";
+                    }
+                ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="subject">投票主題：</label>
+            </td>
+            <td>
+                <input type="text" name="subject" id="subject">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="">單/複選：</label>
+            </td>
+            <td>
+                <input type="radio" name="multiple" value="0" checked>
+                <label>單選</label>
+                <input type="radio" name="multiple" value="1" >
+                <label>複選</label>
+            </td>
+        </tr>
+        <tr>
+            <td>開始時間：</td>
+            <td><input type="date" name="start"></td>
+        </tr>
+        <tr>
+            <td>結束時間：</td>
+            <td><input type="date" name="end"></td>
+        </tr>
+        <!-- 使用表格群組tbody -->
+        <tbody id="options">
+            <tr>
+                <td>
+                    <label>選項:</label>
+                </td>
+                <td>
+                    <input type="text" name="option[]"><input type="button" value="新增選項" id="addvote">
+                </td>
+            </tr>
+        </tbody>
+    </table>
     
-</script>
+    <input type="submit" value="新增">
+    
+    </form>
+    
+    <script>
+        // 動態新增欄位id編號預設值
+        let trId = 1;
+
+        // 動態新增欄位
+        $("#addvote").click(function () {
+            $("#options").append('<tr id="tr'+trId+'"><td><label>選項:</label></td><td><input type="text" name="option[]"><button type="button" onclick="delbtn(' + trId + ')">刪除</button></td></tr>');
+            trId++;
+        });
+        
+        // 動態刪除欄位
+        function delbtn(id) {
+            $("#tr" + id).remove();
+        }    
+        
+    </script>
