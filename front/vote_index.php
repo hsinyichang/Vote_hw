0b6d79818c80d577f7c7bfd748434b036ec6fc16
HsinYiChang <f1233772002@gmail.com>
2022-07-05 11:54:26 +0800

    新增我要投票按鈕

--- front/vote_index.php
+++ front/vote_index.php
@@ -6,6 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
 </head>
 <style>
 .one{
@@ -18,11 +22,21 @@
 }
 .two{
     text-align: center;
-    margin-bottom: 10px;
+    margin:0 auto 10px auto;
+    border: 1px solid black;
+    border-radius: 20px;
+    width: 120px;
+    animation:slideInDown;
+    animation-duration: 0.8s;
+    
 }
 .three{
     display: flex;
     
+}
+.cont:hover{
+    animation:pulse;
+    animation-duration:0.5s ;
 }
 .cont{
     width: calc(100% / 3);
@@ -69,6 +83,7 @@ $recent=all('subjects',$order2.$limit);
                 <li>投票人數：<?=$total;?>人</li>
                 <li>開始時間：<?=$start;?></li>
                 <li>結束時間：<?=$end;?></li>
+                <button type="button">我要投票</button>
             </ul>
             </div>
             
@@ -98,6 +113,7 @@ $recent=all('subjects',$order2.$limit);
                 <li>投票人數：<?=$total;?>人</li>
                 <li>開始時間：<?=$start;?></li>
                 <li>結束時間：<?=$end;?></li>
+                <button type="button">我要投票</button>
             </ul>
             </div>
             
