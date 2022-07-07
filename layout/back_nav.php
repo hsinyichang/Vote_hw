cfd4ed402d331f38adc5a47e7518abeffb864c63
HsinYiChang <f1233772002@gmail.com>
2022-07-07 15:02:21 +0800

    修改有無登入狀態可顯示的標題

--- layout/back_nav.php
+++ layout/back_nav.php
@@ -20,6 +20,6 @@
     <?php
     }
     ?>
-    <div><a href="./index.php"><i class="fa-solid fa-circle-user"></i>前台登入</a></div>  <!--沒登入狀態-->
+    <div><a href="./index.php"><i class="fa-solid fa-circle-user"></i>前台首頁</a></div>  <!--沒登入狀態-->
 
 </nav>
