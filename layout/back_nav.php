1afd9a0388f655d5136ba3a73c242a75feec4909
HsinYiChang <f1233772002@gmail.com>
2022-07-07 15:01:03 +0800

    修改有無登入狀態可顯示的標題

--- layout/back_nav.php
+++ layout/back_nav.php
@@ -1,17 +1,25 @@
 <nav>
-        <div><a href="back.php"><i class="fa-solid fa-bars"></i>投票列表</a></div>
+        <?php
+        if(isset($_SESSION['admin'])){ //有登入才顯示
+        ?>
+        <div><a href="back.php"><i class="fa-solid fa-house"></i>回首頁</a></div>
         <div><a href="?do=add_vote"><i class="fa-solid fa-plus"></i>新增投票</a></div>
         <div><a href="?do=admin_type"><i class="fa-solid fa-database"></i>分類管理</a></div>
-        <?php if(isset($_SESSION['admin'])){
+        <?php
+        }
+        ?>
+        
+        <?php 
+        if(isset($_SESSION['admin'])){
         ?>
             <div><a href="?do=logout"><i class="fa-solid fa-circle-user"></i>登出</a></div>
         <?php
-        }else{
+        }else{//沒登入狀態
         ?>
         <div><a href="?do=login"><i class="fa-solid fa-circle-user"></i>登入</a></div>
     <?php
     }
     ?>
-    <div><a href="./index.php"><i class="fa-solid fa-circle-user"></i>前台登入</a></div>
+    <div><a href="./index.php"><i class="fa-solid fa-circle-user"></i>前台登入</a></div>  <!--沒登入狀態-->
 
-        </nav>
+</nav>
