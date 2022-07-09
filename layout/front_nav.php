604b5dea65d3dabf620d6735566a6651d762b676
HsinYiChang <f1233772002@gmail.com>
2022-07-10 00:25:20 +0800

    dropdown-menu

--- layout/front_nav.php
+++ layout/front_nav.php
@@ -5,8 +5,15 @@
         // include_once "connect.php";/*放到程式開頭，因裡面有session start */
         if(isset($_SESSION['user'])){/*這裡判斷是否有session為登出或登入 */
         ?>
-    <div><a href='?do=member_center'><i class="fa-solid fa-circle-user"></i>會員中心</a>
-         <a href='?do=logout'><i class="fa-solid fa-circle-user"></i>登出</a>
+    <div>
+        <a href="#" id="membercenter"><i class="fa-solid fa-circle-user"></i>會員中心</a>
+        <div class="dropdown-menu">
+            <li><a href="?do=member_center">編輯會員資料</a></li>
+            <li><a href="?do=vote_log">查看已投票主題</a></li>
+            <li><hr></li>
+            <li><a href='?do=logout'><i class="fa-solid fa-circle-user"></i>登出</a></li>
+        </div>
+        
     </div>
         <?php
         }else{
@@ -26,5 +33,16 @@
            
         }
         ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            $('#membercenter').click(function(){
+                $(this).next().toggle();  
+                //next 同層的下一個  都在div裡 h1下一個就是ul
+                //children() 小孩層 最接近 li
+            });
+        </script>
         
 </nav>
