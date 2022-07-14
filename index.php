ed3d57776c31ce58b5084fae3eb9d1b1504c6bb2
HsinYiChang <f1233772002@gmail.com>
2022-07-14 22:34:22 +0800

    將link移至首頁

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
     <link rel="stylesheet" href="./css/basic.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.17/sweetalert2.min.css"
+        integrity="sha512-CJ5goVzT/8VLx0FE2KJwDxA7C6gVMkIGKDx31a84D7P4V3lOVJlGUhC2mEqmMHOFotYv4O0nqAOD0sEzsaLMBg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <div id="header">    
@@ -23,15 +26,15 @@
 <div id="container">
     <?php
     include "./layout/slider.php";
-if(isset($_GET['do'])){ //在vote_list頁面設do變數125行開始
-    $file='./front/'.$_GET['do'].".php";
-}
-if(isset($file) && file_exists($file)){
-    include $file;
-}else{
-    include "./front/vote_index.php";
-}
-?>
+    if(isset($_GET['do'])){ //在vote_list頁面設do變數125行開始
+        $file='./front/'.$_GET['do'].".php";
+    }
+    if(isset($file) && file_exists($file)){
+        include $file;
+    }else{
+        include "./front/vote_index.php";
+    }
+    ?>
 </div>
     <?php include "./layout/footer.php";?>  
 </body>
